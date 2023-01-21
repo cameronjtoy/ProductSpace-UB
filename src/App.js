3ffcba1866c7358ead61c2939757101db7f24cfa
lyao <lyao3@buffalo.edu>
2023-01-21 11:59:11 -0500

    created more components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from "./Components/Header/Header.jsx"
 import { Navbar } from "./Components/Navbar/Navbar.jsx"
+import { Home } from "./Components/Home/Home.jsx"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <Header/>
     </div>
     
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+export const Home = () => {
+    return(
+        <div>
+            <div class="text-jost text-gray-700 mx-40 text-center">
+                <h3 class="text-3xl py-3" >What is Product Space @ UB?</h3>
+                <span class="text-xl">Product Space @ UB is the University at Buffalo chapter of Product Space, a nation-wide organization of students who are passionate about product management. ETC </span>
+            </div>
+
+            <div>
+                For Students
+            </div>
+
+            <div>
+                For Companies
+            </div>
+
+
+
+        </div>
+        
+    )
+}
