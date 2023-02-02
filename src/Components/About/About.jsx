7066e86939f2b2367da5c7a4c420271dedbc6cee
cameronjtoy <70675026+cameronjtoy@users.noreply.github.com>
2023-02-02 18:02:34 -0500

    flip card changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,15 @@
 import './index.css';
 import luffy from '../../Assets/images/luffy.png';
+import FlipCard from '../FlipCard';
 
 const card = [
+    {
+        id: 1,
+        img: luffy,
+        name: "Luffy",
+        role: "President",
+        linkedin: "LinkedIn"
+    },
 ]
 
 
@@ -9,7 +17,6 @@ const card = [
 const About = () => {
     return (
         <div>
-<<<<<<< HEAD
             <div class="about">
                 <div class="container top-padding">
                     <div class="row align-items-center">
@@ -28,20 +35,11 @@ const About = () => {
             </div>
             <div class="leadership">
                 <h1>Leadership</h1>
-                {/* Flip Card  */}
-                <div class="flex min-h-screen flex-col justify-center">
-                    <div class="group h-96 w-96 [perspective:1080px]">
-                        <div class="relative h-full w-full  rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-                            <div class="absolute inset-0"></div>
-                                <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40 " src={luffy} alt="" />
-                            </div>
-                            <div class="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden] [transform-style:preserve-3d] group-hover:[transform:rotateY(0deg)]">
-                            <h1 class="text-2xl font-medium not-italic mt-20  font-sans:">Luffy</h1>
-                            <p class="text-xl font-medium not-italic mb-30 font-sans:">Pirate King</p>
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white mt-20 font-bold py-2 px-4 rounded-full">Contact</button>
-                        </div>
-                    </div>
-                </div>
+                    {
+                        card.map((card) => {
+                            return <FlipCard key={card.id} card={card} />
+                        })
+                    }
             </div>
 
             <div class="beyond-product-space">
@@ -50,9 +48,6 @@ const About = () => {
             </div>
         </div>
 
-=======
-        </div>
->>>>>>> origin/main
     );
 
 }
--- src/Components/FlipCard.jsx
+++ src/Components/FlipCard.jsx
@@ -1,16 +1,15 @@
-function FlipCard(){
+function FlipCard({card}){
     return (
-        <div className="flip-card-outer">
-            <div className="flip-card-inner">
-                <div className="flip-card-front">
-                    <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                        <p className="card-text">Some text</p>
+        <div className="flex min-h-screen flex-col justify-center">
+            <div className="group h-96 w-96 [perspective:1080px]">
+                <div className="relative h-full w-full  rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+                    <div className="absolute inset-0"></div>
+                        <img className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40 " src={card.img} alt="" />
                     </div>
-                </div>
-                <div className="flip-card-back">
-                    <div className="card-body d-flex flex-column justify-content-center align-items-center">
-                        <p className="card-text">Some text</p>
-                    </div>
+                    <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden] [transform-style:preserve-3d] group-hover:[transform:rotateY(0deg)]">
+                    <h1 className="text-2xl font-medium not-italic mt-20  font-sans:">{card.name}</h1>
+                    <p className="text-xl font-medium not-italic mb-30 font-sans:">{card.role}</p>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white mt-20 font-bold py-2 px-4 rounded-full">{card.linkedin}</button>
                 </div>
             </div>
         </div>
